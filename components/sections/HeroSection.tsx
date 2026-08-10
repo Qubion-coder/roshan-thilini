1ad908b3a7c3bc50a9190d1efe4954246412024a
lavanyacheshani <lavanyacheshani5@gmail.com>
2026-08-10 19:38:02 +0530

    Update wedding details for Ashini and Kushan

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -4,7 +4,7 @@ export default function HeroSection() {
   return (
     <section className="relative min-h-[100dvh] w-full overflow-hidden">
       <div
-        className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+        className="absolute inset-0 bg-cover bg-top bg-no-repeat"
         style={{ backgroundImage: 'url("/ChatGPT Image Aug 10, 2026, 07_27_29 PM.png")' }}
       />
     </section>
